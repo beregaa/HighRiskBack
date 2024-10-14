3ecede6488ec10939e9366c77b1cfe26413d4261
beregaa <105544549+beregaa@users.noreply.github.com>
2024-10-14 17:29:41 +0400

    changed db config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,29 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
+    // TypeOrmModule.forRoot({
+    //     type: 'mysql',
+    //     host: 'reversedb.c3kuaa2ogp2y.eu-north-1.rds.amazonaws.com',
+    //     port: 3306,
+    //     username: 'admin',
+    //     password: 'mindArchitacture3000',
+    //     database: 'reverseDB',
+    //     entities: [],
+    //     synchronize: true,
+    //   }),
+
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'reversedb.c3kuaa2ogp2y.eu-north-1.rds.amazonaws.com',
-      port: 3306,
-      username: 'admin',
-      password: 'mindArchitacture3000',
-      database: 'reverseDB',
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
       entities: [],
       synchronize: true,
     }),
