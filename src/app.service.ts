aa0b1d151c98848daa2bc46fe5a605b1537a0758
beregaa <105544549+beregaa@users.noreply.github.com>
2025-02-11 16:50:56 +0400

    deploy testing

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'lets gooo all brands will be outplayed , berega and thorfinn';
+    return 'lets gooo all brands will be outplayed ,berega and thorfinn';
   }
 }
