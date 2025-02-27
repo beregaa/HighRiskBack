50ae993d397411d6fcc306893cb47cf076a3c62a
beregaa <105544549+beregaa@users.noreply.github.com>
2025-02-27 22:36:43 +0400

    chor

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -36,6 +36,6 @@ export class AuthController {
       googleAuth: true,
     });
 
-    res.redirect(`https://high-risk-front.vercel.app/?token=${response.accessToken}`);
+    res.redirect(`http://localhost:3000/?token=${response.accessToken}`);
   }
 }
