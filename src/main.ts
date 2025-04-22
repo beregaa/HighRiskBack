c129c00e389be5fd5100d3564875ada5ea454e0c
beregaa <105544549+beregaa@users.noreply.github.com>
2025-04-22 17:28:52 +0400

    a

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 // main.ts
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   // console.log('ENV:', process.env);
