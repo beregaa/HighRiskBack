9f08ba6d1b182aff0b6e1f77470ce91ea7dfc632
beregaa <105544549+beregaa@users.noreply.github.com>
2025-04-22 17:24:23 +0400

    final final test

--- src/main.ts
+++ src/main.ts
@@ -4,17 +4,18 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  // console.log('ENV:', process.env);
+  // const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      transform: true,
-    }),
-  );
+  // app.useGlobalPipes(
+  //   new ValidationPipe({
+  //     whitelist: true,
+  //     transform: true,
+  //   }),
+  // );
 
  
-  app.enableCors({ origin: '*' })
-  await app.listen(3001);
+  // app.enableCors({ origin: '*' })
+  // await app.listen(3001);
 }
 bootstrap();
